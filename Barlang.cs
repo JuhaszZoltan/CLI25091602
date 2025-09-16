9a8638fd8dba5b45e74129a2d9f071ec1ad3f6af
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-09-16 13:24:20 +0200

    Add project files.

---
+++ Barlang.cs
@@ -0,0 +1,29 @@
+﻿public class Barlang
+{
+    public int Azon { get; set; }
+    public string Nev { get; set; }
+    public int Hossz { get; set; }
+    public int Melyseg { get; set; }
+    public string Telepules { get; set; }
+    public string Vedettseg { get; set; }
+
+    public override string ToString()
+    {
+        return $"Név: {Nev}\n" +
+            $"Hossz: {Hossz} m\n" +
+            $"Mélység: {Melyseg} m\n" +
+            $"Település: {Telepules}\n" +
+            $"VF: {Vedettseg}";
+    }
+
+    public Barlang(string sor)
+    {
+        var v = sor.Split(';');
+        Azon = int.Parse(v[0]);
+        Nev = v[1];
+        Hossz = int.Parse(v[2]);
+        Melyseg = int.Parse(v[3]);
+        Telepules = v[4];
+        Vedettseg = v[5];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+const string file = "..\\..\\..\\resources\\barlangok.txt";
+
+List<Barlang> barlangok = [];
+
+using StreamReader sr = new(file, Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) barlangok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"barlangok szama: {barlangok.Count}");
+
+var mavg = barlangok
+    .Where(b => b.Telepules.StartsWith("Miskolc"))
+    .Average(b => b.Melyseg);
+Console.WriteLine($"miskolci atlag melyseg: {mavg:0.000} m");
+
+Console.Write("írd be a vf-et: ");
+var kervsz = Console.ReadLine();
+var lhvszb = barlangok
+    .Where(b => b.Vedettseg == kervsz)
+    .MaxBy(b => b.Hossz);
+if (lhvszb is null) Console.WriteLine("nincs ilyen");
+else Console.WriteLine(lhvszb);
